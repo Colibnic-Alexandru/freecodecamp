fa8d4e02001294275e5de871c9398fd15a4ef171
AlexandruColibnic <59502870+AlexandruColibnic@users.noreply.github.com>
2022-02-21 12:18:41 +0200

    task 16-30

---
+++ 21.js
@@ -0,0 +1,8 @@
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a += 12;
+b = b += 9;
+c = c += 7;
---
+++ 23.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a = a *= 5;
+b = b *= 3;
+c = c *= 10;
